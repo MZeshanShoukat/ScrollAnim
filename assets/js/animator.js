f095f798570f96a101c81cfcda6bd180c4dc0604
j <j@localhost.localdomain>
2021-08-17 00:32:30 -0400

    putstate_method and block-css

---
+++ assets/js/animator.js
@@ -0,0 +1,21 @@
+let putState = (state) => {
+  for(let item of state){
+    let el = document.getElementById(item.id);
+    el.style.left = item.x;
+    el.style.top = item.y;
+    el.style.width = item.width;
+  }
+};
+
+
+$(document).ready(function(){
+  putState(origin_state);
+  $(document).scroll(function(){
+    // var x = $(document).scrollTop();
+    // $('#c1p1').css("transform", `translate(${(x/52) - 20}px, ${-x/30 - 50}px) rotate(${-x/500}deg)`);
+    // $('#c1p2').css("transform", `translate(${(x)/50 + 100}px, ${((-x)/40) - 120}px) rotate(${x/x - 12}deg)`);
+    // $('#c1p3').css("transform", `translate(${(x)/50 + 25}px, ${((-x)/38) - 50}px) rotate(15deg)`);
+    // $('#c1p4').css("transform", `translate(${(x)/40 + 80}px, ${((-x)/43) + 30}px) rotate(${x/500}deg)`);
+    // $('#c1p5').css("transform", `translate(${(x)/28 - 50}px, ${((-x)/33) - 30}px) rotate(${-x/100}deg)`);
+  });
+});
---
+++ assets/js/origin_states.js
@@ -0,0 +1,92 @@
+let origin_state = [
+  {
+    id: "c1p1",
+    x: '-15vw',
+    y: '1vw',
+    width: '50vw'
+  },
+  {
+    id: "c1p2",
+    x: '-15vw',
+    y: '1vw',
+    width: '50vw'
+  },
+  {
+    id: "c1p3",
+    x: '-15vw',
+    y: '1vw',
+    width: '50vw'
+  },
+  {
+    id: "c1p4",
+    x: '-15vw',
+    y: '1vw',
+    width: '50vw'
+  },
+  {
+    id: "c1p5",
+    x: '-15vw',
+    y: '1vw',
+    width: '50vw'
+  },
+  {
+    id: "c1p6",
+    x: '-15vw',
+    y: '1vw',
+    width: '50vw'
+  },
+  {
+    id: "c1p7",
+    x: '-15vw',
+    y: '1vw',
+    width: '50vw'
+  },
+  {
+    id: "c1p8",
+    x: '-15vw',
+    y: '1vw',
+    width: '50vw'
+  },
+  {
+    id: "c1p9",
+    x: '-15vw',
+    y: '1vw',
+    width: '50vw'
+  },
+  {
+    id: "c1pp10",
+    x: '-15vw',
+    y: '1vw',
+    width: '50vw'
+  },
+  {
+    id: "c1ppp10",
+    x: '-15vw',
+    y: '1vw',
+    width: '50vw'
+  },
+  {
+    id: "c5p1",
+    x: '-15vw',
+    y: '1vw',
+    width: '50vw'
+  },
+  {
+    id: "c3p1",
+    x: '-15vw',
+    y: '1vw',
+    width: '50vw'
+  },
+  {
+    id: "c2p1",
+    x: '-15vw',
+    y: '1vw',
+    width: '50vw'
+  },
+  {
+    id: "c4p1",
+    x: '-15vw',
+    y: '1vw',
+    width: '50vw'
+  },
+];
