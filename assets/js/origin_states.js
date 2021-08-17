f8abba74bad4ffd26fbb139b754f36bec96a6267
j <j@localhost.localdomain>
2021-08-17 19:34:17 -0400

    Updated vw to %age

--- assets/js/origin_states.js
+++ assets/js/origin_states.js
@@ -1,107 +1,107 @@
 let origin_state = [
   {
     id: "c1p1",
-    x: '27vw',
-    y: '20vw',
-    width: '20vw',
+    x: '27%',
+    y: '20%',
+    width: '20%',
     rotate: '-6deg'
   },
   {
     id: "c1p2",
-    x: '22vw',
-    y: '20vw',
-    width: '8vw',
+    x: '22%',
+    y: '20%',
+    width: '8%',
     rotate: '-12deg'
   },
   {
     id: "c1p3",
-    x: '26vw',
-    y: '23vw',
-    width: '4vw',
+    x: '26%',
+    y: '23%',
+    width: '4%',
     rotate: '10deg'
   },
   {
     id: "c1p4",
-    x: '24.38vw',
-    y: '23vw',
-    width: '4vw',
+    x: '24.38%',
+    y: '23%',
+    width: '4%',
     rotate: '-16deg'
   },
   {
     id: "c1p5",
-    x: '20vw',
-    y: '21vw',
-    width: '2vw',
+    x: '20%',
+    y: '21%',
+    width: '2%',
     rotate: '-42deg'
   },
   {
     id: "c1p6",
-    x: '20.5vw',
-    y: '23vw',
-    width: '5vw',
+    x: '20.5%',
+    y: '23%',
+    width: '5%',
     rotate: '-13deg'
   },
   {
     id: "c1p7",
-    x: '16vw',
-    y: '22vw',
-    width: '4vw',
+    x: '16%',
+    y: '22%',
+    width: '4%',
     rotate: '0deg'
   },
   {
     id: "c1p8",
-    x: '16vw',
-    y: '23.2vw',
-    width: '5vw',
+    x: '16%',
+    y: '23.2%',
+    width: '5%',
     rotate: '-56deg'
   },
   {
     id: "c1p9",
-    x: '8vw',
-    y: '24.5vw',
-    width: '20vw',
+    x: '8%',
+    y: '24.5%',
+    width: '20%',
     rotate: '-32deg'
   },
   {
     id: "c1pp10",
-    x: '46vw',
-    y: '24vw',
-    width: '4vw',
+    x: '46%',
+    y: '24%',
+    width: '4%',
     rotate: '-4deg'
   },
   {
     id: "c1ppp10",
-    x: '47vw',
-    y: '22vw',
-    width: '2vw',
+    x: '47%',
+    y: '22%',
+    width: '2%',
     rotate: '0deg'
   },
   {
     id: "c5p1",
-    x: '33vw',
-    y: '27vw',
-    width: '28vw',
+    x: '33%',
+    y: '27%',
+    width: '28%',
     rotate: '0deg'
   },
   {
     id: "c3p1",
-    x: '26vw',
-    y: '27vw',
-    width: '10vw',
+    x: '26%',
+    y: '27%',
+    width: '10%',
     rotate: '-16deg'
   },
   {
     id: "c2p1",
-    x: '20vw',
-    y: '37vw',
-    width: '16vw',
+    x: '20%',
+    y: '37%',
+    width: '16%',
     rotate: '0deg'
   },
   {
     id: "c4p1",
-    x: '36vw',
-    y: '33vw',
-    width: '25vw',
+    x: '36%',
+    y: '33%',
+    width: '25%',
     rotate: '0deg'
   },
 ];
