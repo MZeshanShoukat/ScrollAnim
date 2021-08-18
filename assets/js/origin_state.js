dc98ee2d2c0cab4c758e0a91966605139dda6960
j <j@localhost.localdomain>
2021-08-18 00:27:41 -0400

    Added Animation-Code

---
+++ assets/js/origin_state.js
@@ -0,0 +1,107 @@
+let origin_state = [
+  {
+    id: "c1p1",
+    x: 43,
+    y: 47,
+    width: 21,
+    rotate: 1
+  },
+  {
+    id: "c1p2",
+    x: 38.2,
+    y: 44,
+    width: 10,
+    rotate: -5
+  },
+  {
+    id: "c1p3",
+    x: 41.5,
+    y: 49,
+    width: 5,
+    rotate: 4
+  },
+  {
+    id: "c1p4",
+    x: 40,
+    y: 50,
+    width: 6,
+    rotate: 0
+  },
+  {
+    id: "c1p5",
+    x: 38,
+    y: 47.2,
+    width: 2,
+    rotate: -42
+  },
+  {
+    id: "c1p6",
+    x: 36,
+    y: 49,
+    width: 6,
+    rotate: 1
+  },
+  {
+    id: "c1p7",
+    x: 35,
+    y: 44,
+    width: 4,
+    rotate: -48
+  },
+  {
+    id: "c1p8",
+    x: 27,
+    y: 42,
+    width: 5,
+    rotate: -32
+  },
+  {
+    id: "c1p9",
+    x: 14,
+    y: 42,
+    width: 28,
+    rotate: -32
+  },
+  {
+    id: "c1pp10",
+    x: 63,
+    y: 54,
+    width: 4,
+    rotate: -22
+  },
+  {
+    id: "c1ppp10",
+    x: 64,
+    y: 52,
+    width: 2,
+    rotate: 0
+  },
+  {
+    id: "c5p1",
+    x: 43,
+    y: 58,
+    width: 38,
+    rotate: 0
+  },
+  {
+    id: "c3p1",
+    x: 36,
+    y: 61,
+    width: 18,
+    rotate: -22
+  },
+  {
+    id: "c2p1",
+    x: 25,
+    y: 77,
+    width: 22,
+    rotate: -4
+  },
+  {
+    id: "c4p1",
+    x: 50,
+    y: 69,
+    width: 38,
+    rotate: -33
+  },
+];
