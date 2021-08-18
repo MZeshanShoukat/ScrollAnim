aa132f3b970b6468349c2367d2310bf7b23dab58
j <j@localhost.localdomain>
2021-08-18 01:46:13 -0400

    Nearly Finalized

--- assets/js/animator.js
+++ assets/js/animator.js
@@ -1,6 +1,8 @@
+// Variables to Use
 let scroll_height = 0,
     current_ratio = 0;
 
+// apply given cords & transformations to continents (images)
 let putState = (state) => {
   for(let item of state){
     let el = document.getElementById(item.id);
@@ -11,14 +13,61 @@ let putState = (state) => {
   }
 };
 
+function apply_globe_visibility(current_ratio){
+  if(current_ratio<0.8){
+    $("#cont-section").addClass("visible");
+  } else {
+    $("#cont-section").removeClass("visible");
+  }
+}
+
+function apply_typo_visibility(current_ratio){
+  // Text: "Imaging"
+  if(current_ratio<0.5){
+    $("#wrd2").removeClass("visible");
+  } else {
+    $("#wrd2").addClass("visible");
+  }
+
+  // Text: "no"
+  if(current_ratio<0.75){
+    $("#wrd3p1").removeClass("visible");
+  } else {
+    $("#wrd3p1").addClass("visible");
+  }
+
+  // Text: "boundries"
+  if(current_ratio<0.9){
+    $("#wrd3p2").removeClass("visible");
+  } else {
+    $("#wrd3p2").addClass("visible");
+  }
+}
+
 $(document).ready(function(){
-  putState(origin_state);
+  // Put Start-State Initially
+  putState(start_state);
+
+  // Calculate and store scrollable-scroll-hieght
   scroll_height = document.body.scrollHeight - window.innerHeight;
 
   $(document).scroll(function(){
+    // Current-Position to Ent ratio
     current_ratio = window.scrollY / scroll_height;
-    console.log(scroll_height, window.scrollY, current_ratio);
 
+    // Change Ratio
+    apply_globe_visibility(current_ratio);
+
+    // Change Ratio
+    current_ratio = current_ratio + 0.3;
+    if(current_ratio>1){
+      current_ratio = 1;
+    }
+
+    // Teypography Visibility
+    apply_typo_visibility(current_ratio);
+
+    // Calculate Current State
     let current_state = JSON.parse(JSON.stringify(start_state));
     for(let i in start_state){
       ['x', 'y', 'width', 'rotate'].forEach(item => {
@@ -28,6 +77,8 @@ $(document).ready(function(){
           current_ratio;
       });
     }
-    // putState(current_state);
+
+    // Apply Current Cords to Images
+    putState(current_state);
   });
 });
--- assets/js/start_state.js
+++ assets/js/start_state.js
@@ -1,107 +1,107 @@
 let start_state = [
   {
     id: "c1p1",
-    x: 43,
-    y: 47,
+    x: 12,
+    y: 60,
     width: 21,
-    rotate: 1
+    rotate: 0
   },
   {
     id: "c1p2",
-    x: 38.2,
-    y: 44,
+    x: 6,
+    y: 57,
     width: 10,
     rotate: -5
   },
   {
     id: "c1p3",
-    x: 41.5,
-    y: 49,
+    x: 7,
+    y: 65,
     width: 5,
-    rotate: 4
+    rotate: 13
   },
   {
     id: "c1p4",
-    x: 40,
-    y: 50,
+    x: 4,
+    y: 66,
+    width: 6,
+    rotate: -5
+  },
+  {
+    id: "c1p5",
+    x: 1,
+    y: 59,
+    width: 2.4,
+    rotate: -6
+  },
+  {
+    id: "c1p6",
+    x: -1,
+    y: 66,
     width: 6,
     rotate: 0
   },
   {
-    id: "c1p5",
-    x: 38,
-    y: 47.2,
-    width: 2,
-    rotate: -42
-  },
-  {
-    id: "c1p6",
-    x: 36,
-    y: 49,
-    width: 6,
-    rotate: 1
-  },
-  {
     id: "c1p7",
-    x: 35,
-    y: 44,
+    x: -4,
+    y: 61,
     width: 4,
-    rotate: -48
+    rotate: 0
   },
   {
     id: "c1p8",
-    x: 27,
-    y: 42,
-    width: 5,
-    rotate: -32
+    x: -6,
+    y: 67,
+    width: 6,
+    rotate: 0
   },
   {
     id: "c1p9",
-    x: -1,
-    y: 42,
+    x: -23,
+    y: 69,
     width: 28,
-    rotate: -32
+    rotate: 0
   },
   {
     id: "c1pp10",
-    x: 63,
-    y: 54,
-    width: 4,
-    rotate: -22
+    x: 54,
+    y: 82,
+    width: 6,
+    rotate: -60
   },
   {
     id: "c1ppp10",
-    x: 64,
-    y: 52,
-    width: 2,
+    x: 52,
+    y: 80,
+    width: 3,
     rotate: 0
   },
   {
     id: "c5p1",
-    x: 43,
-    y: 58,
+    x: 45,
+    y: 88,
     width: 38,
     rotate: 0
   },
   {
     id: "c3p1",
-    x: 36,
-    y: 61,
+    x: 38,
+    y: 104,
     width: 18,
-    rotate: -22
+    rotate: 0
   },
   {
     id: "c2p1",
-    x: 25,
-    y: 77,
+    x: 8,
+    y: 115,
     width: 22,
-    rotate: -4
+    rotate: 0
   },
   {
     id: "c4p1",
-    x: 50,
-    y: 69,
+    x: 69,
+    y: 118,
     width: 38,
-    rotate: -33
+    rotate: 0
   },
 ];
